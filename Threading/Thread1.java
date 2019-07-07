e9453ab1cbbc9b8b5bac4c38d9848e72b80c0cfc
Apoorv Garg <u.garg.10@gmail.com>
2019-07-07 21:19:59 +0530

    Add files via upload

---
+++ Threading/Thread1.java
@@ -0,0 +1,8 @@
+class Thread1
+{
+public static void main(String s[])
+	{
+	Thread t=Thread.currentThread();
+	System.out.println(t.getName());
+	}
+}
---
+++ Threading/Thread_getname.java
@@ -0,0 +1,8 @@
+class Thread_getname
+{
+public static void main(String s[])
+	{
+	Thread t=Thread.currentThread();
+	System.out.println(t.getName());
+	}
+}
