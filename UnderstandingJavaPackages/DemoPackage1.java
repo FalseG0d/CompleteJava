2cfc3f6308eef0769f0ee21e7d53b672b243ecb0
Apoorv Garg <u.garg.10@gmail.com>
2019-07-17 15:32:59 +0530

    Add files via upload

---
+++ UnderstandingJavaPackages/DemoPackage1.java
@@ -0,0 +1,12 @@
+package p1;
+public class DemoPackage1
+{
+public void show()
+	{
+	System.out.println("DemoPackage1.1");
+	}
+public static void main(String s[])
+	{
+	new DemoPackage1().show();
+	}
+}
