3be6d741ad8c47fd00c5e7bc0bdcf4726c7c95a9
Apoorv Garg <u.garg.10@gmail.com>
2019-07-22 17:59:59 +0530

    Add files via upload

---
+++ Misceleneous/Array_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Array_2
+{
+public static void main(String s[])
+	{
+	int x[][];
+	x=new int[3][];
+	System.out.println(x);
+	for(int i=0;i<3;i++)
+		{
+		//for(int j=0;j<3;j++)
+			x[i]=new /*Scanner(System.in).nextInt()*/int[3];
+			System.out.println(x[i]);
+		}
+	}
+	
+}
